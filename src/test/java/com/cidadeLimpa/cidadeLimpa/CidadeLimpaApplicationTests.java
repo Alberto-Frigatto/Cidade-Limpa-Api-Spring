bbf38ea66609dd3652a73b5979dca44dc0dde949
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-05-17 10:29:41 -0300

    feat: Adicionar configuração inicial do projeto
    Adiciona a estrutura de diretórios e arquivos fornecidos pelo spring initializr.

---
+++ src/test/java/com/cidadeLimpa/cidadeLimpa/CidadeLimpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cidadeLimpa.cidadeLimpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidadeLimpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
