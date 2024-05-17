acb3ea4aca80655d9aa9f73948411e1af8275ec6
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-05-17 10:31:19 -0300

    feat: Adicionar classe Lixeira ao pacote models

---
+++ src/main/java/com/cidadeLimpa/cidadeLimpa/model/Lixeira.java
@@ -0,0 +1,51 @@
+package com.cidadeLimpa.cidadeLimpa.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_lixeira")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Lixeira
+{
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "SEQ_LIXEIRA"
+    )
+    @SequenceGenerator(
+            name = "SEQ_LIXEIRA",
+            sequenceName = "SEQ_LIXEIRA",
+            allocationSize = 50
+    )
+    @Column(name = "id_lixeira")
+    private Long idLixeira;
+
+    @Column(nullable = false, length = 100)
+    private String localizacao;
+
+    @Column(nullable = false)
+    private Integer capacidade;
+
+    @Column(nullable = false)
+    private Integer tipo;
+
+    @Column(nullable = false, precision = 3, scale = 2)
+    private BigDecimal ocupacao;
+}
