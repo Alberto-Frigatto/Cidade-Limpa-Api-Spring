42c63f9c7b8bc21b333735494a3727f939d8fbf4
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-05-21 01:27:26 -0300

    feat: Criar interface LixeiraParaColetaRepository no pacote repository

---
+++ src/main/java/com/cidadeLimpa/cidadeLimpa/repository/LixeiraParaColetaRepository.java
@@ -0,0 +1,8 @@
+package com.cidadeLimpa.cidadeLimpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.cidadeLimpa.cidadeLimpa.model.LixeiraParaColeta;
+
+public interface LixeiraParaColetaRepository extends JpaRepository<LixeiraParaColeta, Long>  {
+
+}
