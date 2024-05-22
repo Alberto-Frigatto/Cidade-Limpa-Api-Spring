efcb6463c63bec71ff0807dfabdb8fa0e41f1389
Gustavo Lopes <guhrlopes999@gmail.com>
2024-05-21 22:28:09 -0300

    refactor: Alterar classe Rota no pacote models

--- src/main/java/com/cidadeLimpa/cidadeLimpa/model/Rota.java
+++ src/main/java/com/cidadeLimpa/cidadeLimpa/model/Rota.java
@@ -2,13 +2,13 @@ package com.cidadeLimpa.cidadeLimpa.model;
 
 import java.util.ArrayList;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
-import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
