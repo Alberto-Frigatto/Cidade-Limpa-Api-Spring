6d5a1d7e769e57292c603a2972ea84e417eb185e
Gustavo Lopes <guhrlopes999@gmail.com>
2024-05-21 22:27:16 -0300

    feat: Adicionar classe CaminhaoService no pacote service

---
+++ src/main/java/com/cidadeLimpa/cidadeLimpa/service/CaminhaoService.java
@@ -0,0 +1,106 @@
+package com.cidadeLimpa.cidadeLimpa.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cidadeLimpa.cidadeLimpa.dto.CreateCaminhaoDTO;
+import com.cidadeLimpa.cidadeLimpa.dto.DisplayCaminhaoDTO;
+import com.cidadeLimpa.cidadeLimpa.dto.UpdateCaminhaoDTO;
+import com.cidadeLimpa.cidadeLimpa.model.Caminhao;
+import com.cidadeLimpa.cidadeLimpa.model.Rota;
+import com.cidadeLimpa.cidadeLimpa.repository.CaminhaoRepository;
+import com.cidadeLimpa.cidadeLimpa.repository.RotaRepository;
+
+@Service
+public class CaminhaoService
+{
+    @Autowired
+    private CaminhaoRepository caminhaoRepository;
+
+    @Autowired
+    private RotaRepository rotaRepository;
+
+    public List<DisplayCaminhaoDTO> getAllCaminhoes()
+    {
+        return caminhaoRepository
+                    .findAll()
+                    .stream()
+                    .map(DisplayCaminhaoDTO::new)
+                    .toList();
+    }
+
+    public DisplayCaminhaoDTO getCaminhaoById(Long id)
+    {
+        Optional<Caminhao> caminhao = caminhaoRepository.findById(id);
+
+        if (caminhao.isEmpty())
+        {
+            String message = "O caminhão " + id + " não existe";
+
+            throw new RuntimeException(message);
+        }
+
+        return new DisplayCaminhaoDTO(caminhao.get());
+    }
+
+    public DisplayCaminhaoDTO createCaminhao(CreateCaminhaoDTO createCaminhaoDTO)
+    {
+        Caminhao caminhao = new Caminhao();
+
+        Rota rota = this.getRotaById(createCaminhaoDTO.idRota());
+
+        caminhao.setRota(rota);
+        caminhao.setCapacidade(createCaminhaoDTO.capacidade());
+        caminhao.setModelo(createCaminhaoDTO.modelo());
+        caminhao.setPlaca(createCaminhaoDTO.placa());
+
+        Caminhao caminhaoSalvo = caminhaoRepository.save(caminhao);
+
+        return new DisplayCaminhaoDTO(caminhaoSalvo);
+
+    }
+
+    private Rota getRotaById(Long id)
+    {
+        Optional<Rota> rota = rotaRepository.findById(id);
+
+        if (rota.isEmpty())
+        {
+            String message = "A rota " + id + " não existe";
+
+            throw new RuntimeException(message);
+        }
+
+        return rota.get();
+    }
+
+    public void deleteCaminhao(Long id)
+    {
+        DisplayCaminhaoDTO caminhaoDTO = this.getCaminhaoById(id);
+        Caminhao caminhao = new Caminhao();
+
+        BeanUtils.copyProperties(caminhaoDTO, caminhao);
+
+        caminhaoRepository.delete(caminhao);
+    }
+
+    public DisplayCaminhaoDTO updateCaminhao(UpdateCaminhaoDTO updateCaminhaoDTO)
+    {
+        Caminhao caminhao = new Caminhao();
+
+        Rota rota = this.getRotaById(updateCaminhaoDTO.idRota());
+
+        caminhao.setRota(rota);
+        caminhao.setCapacidade(updateCaminhaoDTO.capacidade());
+        caminhao.setModelo(updateCaminhaoDTO.modelo());
+        caminhao.setPlaca(updateCaminhaoDTO.placa());
+
+        Caminhao caminhaoSalvo = caminhaoRepository.save(caminhao);
+
+        return new DisplayCaminhaoDTO(caminhaoSalvo);
+    }
+}
