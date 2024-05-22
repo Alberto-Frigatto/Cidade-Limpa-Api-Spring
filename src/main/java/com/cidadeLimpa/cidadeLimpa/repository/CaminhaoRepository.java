d62e5797f961d21b86101ed49b40f68e56948092
Gustavo Lopes <guhrlopes999@gmail.com>
2024-05-21 22:29:41 -0300

    feat: Adicionar classe CaminhaoRepository no pacote repository

---
+++ src/main/java/com/cidadeLimpa/cidadeLimpa/repository/CaminhaoRepository.java
@@ -0,0 +1,9 @@
+package com.cidadeLimpa.cidadeLimpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cidadeLimpa.cidadeLimpa.model.Caminhao;
+
+public interface CaminhaoRepository extends JpaRepository<Caminhao, Long> {
+
+}
