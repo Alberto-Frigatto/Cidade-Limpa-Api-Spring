6aebbaa3551f4884685ab947f8c4045fdae06c42
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-05-23 20:40:56 -0300

    feat: Adicionar validações em UpdateLixeiraParaColetaDTO

--- src/main/java/com/cidadeLimpa/cidadeLimpa/dto/UpdateLixeiraParaColetaDTO.java
+++ src/main/java/com/cidadeLimpa/cidadeLimpa/dto/UpdateLixeiraParaColetaDTO.java
@@ -1,7 +1,11 @@
 package com.cidadeLimpa.cidadeLimpa.dto;
 
+import jakarta.validation.constraints.Min;
+
 public record UpdateLixeiraParaColetaDTO(
+    @Min(value = 1, message = "O id da lixeira deve ser maior ou igual a 1")
     Long idLixeiraParaColeta,
+
     boolean ativo
 ) {
 
