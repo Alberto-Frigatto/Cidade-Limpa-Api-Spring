62f608ad045e3c99e619ccc8fc65bda2b68b7df3
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-05-23 01:45:21 -0300

    feat: Adicionar validações a UpdateCaminhaoDTO

--- src/main/java/com/cidadeLimpa/cidadeLimpa/dto/UpdateCaminhaoDTO.java
+++ src/main/java/com/cidadeLimpa/cidadeLimpa/dto/UpdateCaminhaoDTO.java
@@ -1,9 +1,24 @@
 package com.cidadeLimpa.cidadeLimpa.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record UpdateCaminhaoDTO(
+    @NotBlank(message = "O id do caminhão é obrigatório")
+    Long idCaminhao,
+
+    @NotBlank(message = "O modelo do caminhão é obrigatório")
+    @Size(min = 1, max = 50, message = "O modelo do caminhão deve ter no máximo 50 caracteres")
     String modelo,
+
+    @NotBlank(message = "A capacidade do caminhão é obrigatório")
     Integer capacidade,
+
+    @NotBlank(message = "A placa do caminhão é obrigatório")
+    @Size(min = 7, max = 7, message = "A placa do caminhão deve ter 7 caracteres")
     String placa,
+
+    @NotBlank(message = "O id da rota do caminhão é obrigatório")
     Long idRota
 ) {
 }
