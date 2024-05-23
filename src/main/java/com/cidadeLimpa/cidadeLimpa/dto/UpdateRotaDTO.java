4199d26b1567643bd0716f85c9ab8bae01bdf222
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-05-23 01:46:00 -0300

    feat: Adicionar validações a UpdateRotaDTO

--- src/main/java/com/cidadeLimpa/cidadeLimpa/dto/UpdateRotaDTO.java
+++ src/main/java/com/cidadeLimpa/cidadeLimpa/dto/UpdateRotaDTO.java
@@ -2,10 +2,23 @@ package com.cidadeLimpa.cidadeLimpa.dto;
 
 import java.util.ArrayList;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 public record UpdateRotaDTO(
+    @NotBlank(message = "O horário de início da rota é obrigatório")
+    @Size(min = 5, max = 5, message = "O horário de início da rota deve estar no formato HH:MM")
     String horarioInicio,
+
+    @NotBlank(message = "O horário de fim da rota é obrigatório")
+    @Size(min = 5, max = 5, message = "O horário de fim da rota deve estar no formato HH:MM")
     String horarioFim,
+
+    @NotEmpty(message = "A lista de pontos de coleta da rota é obrigatória")
     ArrayList<String> listaPontosColeta,
+
+    @NotBlank(message = "O id da rota é obrigatório")
     Long idRota
 ) {
 }
