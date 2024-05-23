eb588e2b4e6a8822dc1846d4150addd9b08750aa
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-05-23 20:56:36 -0300

    feat: Adicionar métodos de pesquisa em LixeiraParaColetaRepository

--- src/main/java/com/cidadeLimpa/cidadeLimpa/repository/LixeiraParaColetaRepository.java
+++ src/main/java/com/cidadeLimpa/cidadeLimpa/repository/LixeiraParaColetaRepository.java
@@ -1,8 +1,16 @@
 package com.cidadeLimpa.cidadeLimpa.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.cidadeLimpa.cidadeLimpa.model.LixeiraParaColeta;
 
 public interface LixeiraParaColetaRepository extends JpaRepository<LixeiraParaColeta, Long>  {
+    List<LixeiraParaColeta> findByAtivo(boolean ativo);
 
+    @Query("SELECT l FROM LixeiraParaColeta l WHERE l.lixeira.idLixeira = :idLixeira")
+    List<LixeiraParaColeta> findByIdLixeira(@Param("idLixeira") Long idLixeira);
 }
