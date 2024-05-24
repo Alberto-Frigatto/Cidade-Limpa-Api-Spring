73b0c4a34ddda64bdcad1b6a213e1519f151691f
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-05-24 17:11:45 -0300

    feat: Criar CreateUsuarioDTO no pacote DTO

---
+++ src/main/java/com/cidadeLimpa/cidadeLimpa/dto/CreateUsuarioDTO.java
@@ -0,0 +1,20 @@
+package com.cidadeLimpa.cidadeLimpa.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CreateUsuarioDTO(
+    @NotBlank(message = "O email do usuário é obrigatório")
+    @Email(message = "O email do usuário é inválido")
+    String email,
+
+    @NotBlank(message = "A senha do usuário é obrigatória")
+    @Size(min = 8, message = "A senha do usuário deve ter pelo menos 8 caracteres")
+    String senha,
+
+    @NotBlank(message = "A role do usuário é obrigatória")
+    String role
+) {
+
+}
