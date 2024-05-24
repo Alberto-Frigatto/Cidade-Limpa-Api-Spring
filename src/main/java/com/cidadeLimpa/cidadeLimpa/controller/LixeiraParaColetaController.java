08b58d98cb8650dd70ea56635a9a0a996dae3528
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-05-23 21:31:56 -0300

    feat: Incrementar LixeiraParaColetaController
    Implementa Paginação em getAllLixeiraParaColeta
    
    Implementa validação bean em createLixeiraParaColeta
    
    Implementa validação bean em updateLixeiraParaColeta
    
    Cria os endpoints searchLixeiraParaColetaByAtivo e searchLixeiraParaColetaByIdLixeira

--- src/main/java/com/cidadeLimpa/cidadeLimpa/controller/LixeiraParaColetaController.java
+++ src/main/java/com/cidadeLimpa/cidadeLimpa/controller/LixeiraParaColetaController.java
@@ -3,6 +3,8 @@ package com.cidadeLimpa.cidadeLimpa.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
@@ -19,6 +22,8 @@ import com.cidadeLimpa.cidadeLimpa.dto.DIsplayLixeiraParaColetaDTO;
 import com.cidadeLimpa.cidadeLimpa.dto.UpdateLixeiraParaColetaDTO;
 import com.cidadeLimpa.cidadeLimpa.service.LixeiraParaColetaService;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping("/lixeirasParaColeta")
 public class LixeiraParaColetaController {
@@ -27,9 +32,9 @@ public class LixeiraParaColetaController {
 
     @GetMapping("")
     @ResponseStatus(HttpStatus.OK)
-    public List<DIsplayLixeiraParaColetaDTO> getAllLixeiraParaColeta()
+    public Page<DIsplayLixeiraParaColetaDTO> getAllLixeiraParaColeta(Pageable pagination)
     {
-        return service.getAllLixeirasParaColeta();
+        return service.getAllLixeirasParaColeta(pagination);
     }
 
     @GetMapping("/{id}")
@@ -41,7 +46,7 @@ public class LixeiraParaColetaController {
 
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
-    public DIsplayLixeiraParaColetaDTO createLixeiraParaColeta(@RequestBody CreateLixeiraParaColetaDTO lixeira)
+    public DIsplayLixeiraParaColetaDTO createLixeiraParaColeta(@Valid @RequestBody CreateLixeiraParaColetaDTO lixeira)
     {
         return service.createLixeiraParaColeta(lixeira);
     }
@@ -55,8 +60,22 @@ public class LixeiraParaColetaController {
 
     @PutMapping("")
     @ResponseStatus(HttpStatus.OK)
-    public DIsplayLixeiraParaColetaDTO updateLixeiraParaColeta(@RequestBody UpdateLixeiraParaColetaDTO lixeira)
+    public DIsplayLixeiraParaColetaDTO updateLixeiraParaColeta(@Valid @RequestBody UpdateLixeiraParaColetaDTO lixeira)
     {
         return service.updateLixeiraParaColeta(lixeira);
+    }
+
+    @GetMapping(value = "", params = "ativo")
+    @ResponseStatus(HttpStatus.OK)
+    public List<DIsplayLixeiraParaColetaDTO> searchLixeiraParaColetaByAtivo(@RequestParam boolean ativo)
+    {
+        return service.searchLixeiraParaColetaByAtivo(ativo);
+    }
+
+    @GetMapping(value = "", params = "idLixeira")
+    @ResponseStatus(HttpStatus.OK)
+    public List<DIsplayLixeiraParaColetaDTO> searchLixeiraParaColetaByIdLixeira(@RequestParam Long idLixeira)
+    {
+        return service.searchLixeiraParaColetaByIdLixeira(idLixeira);
     }
 }
