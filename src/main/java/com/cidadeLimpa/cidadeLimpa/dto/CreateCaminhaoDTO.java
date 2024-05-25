6f4a4766ac5d47f1041936300e2efd32b6a9e4a3
Gustavo Lopes <guhrlopes999@gmail.com>
2024-05-25 00:49:04 -0300

    refactor: Alterar classes DTO para o escopo do caminhao

--- src/main/java/com/cidadeLimpa/cidadeLimpa/dto/CreateCaminhaoDTO.java
+++ src/main/java/com/cidadeLimpa/cidadeLimpa/dto/CreateCaminhaoDTO.java
@@ -8,14 +8,12 @@ public record CreateCaminhaoDTO(
     @Size(min = 1, max = 50, message = "O modelo do caminhão deve ter no máximo 50 caracteres")
     String modelo,
 
-    @NotBlank(message = "A capacidade do caminhão é obrigatório")
     Integer capacidade,
 
     @NotBlank(message = "A placa do caminhão é obrigatório")
     @Size(min = 7, max = 7, message = "A placa do caminhão deve ter 7 caracteres")
     String placa,
 
-    @NotBlank(message = "O id da rota do caminhão é obrigatório")
     Long idRota
 ) {
 }
